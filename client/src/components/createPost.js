92aad491718a1b9ddeee1b58576d1b61f01d98d9
drsheko <drsheko@gmail.com>
2022-08-30 04:26:14 -0500

    add upload-photo to create-post

--- client/src/components/createPost.js
+++ client/src/components/createPost.js
@@ -11,6 +11,8 @@ const CreatePost = () => {
         text:'',
         isPublished:true
     })
+    const [upload , setUpload] = useState(null)
+
 
     const handleChange = (e) => {
         setForm({
@@ -19,24 +21,25 @@ const CreatePost = () => {
         })
     }
 
-
+    const uploadPhoto = async(e)=> { 
+        var file = e.target.files[0]
+        setUpload(file)
+        console.log(upload)
+    }
     const handleFormSubmit = async(e) => {
         e.preventDefault();
         try{
             var url = "http://localhost:3001/api/post"
-            var options = {
-                method : 'Post',
-                headers : {
-                    'Content-Type':'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    "title":form.title,
-                    "text": form.text,
-                    user: user._id
-                })
-
-            }
-            var res = await fetch(url ,options)
+            var formData = new FormData()
+            formData.append('title', form.title);
+            formData.append('text', form.text);
+            formData.append('user', user._id);
+            formData.append('postPhoto', upload)
+            
+            var res = await fetch(url ,{
+                method:'Post',
+                body: formData
+            });
             var data =await res.json()
             navigate('/' , {replace:true})
 
@@ -55,7 +58,10 @@ const CreatePost = () => {
 
                 <label>text:</label>
                 <input value={form.text} name = 'text' onChange={handleChange} />
-            
+                
+                <label className="form-label">Photo</label>
+                <input type="file" class="form-control" name='postPhoto'   onChange={uploadPhoto}  />
+
                 <button type="submit" value="submit">Confirm</button>
             </form>
         
